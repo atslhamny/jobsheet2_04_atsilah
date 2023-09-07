c9f018104c8bacaaed838d4cc29461fbbe657935
atslhamny <atsilahamanyph@gmail.com>
2023-09-07 09:20:30 +0700

    commit this source code to github

---
+++ MyFirstJavaGithub04.java
@@ -0,0 +1,9 @@
+/**
+ * MyFirstJavaGithub04
+ */
+public class MyFirstJavaGithub04 {
+
+    public static void main(String[] args) {
+        System.out.println("HAII SAYA SUDAH DI GITHUB");
+    }
+}
